018c6d6b2f39e5de435281d7f582817de3f267ee
IvanKaratunov <ivankaratunov311@gmail.com>
2025-01-23 17:53:00 +0300

    first comiit

--- Fourier transform.py
+++ Fourier transform.py
@@ -6,12 +6,12 @@ y_values = np.sin(x_values + np.pi)
 fft_values = np.fft.fft(y_values)            
 amplitude = np.abs(fft_values)             
 frequency = np.fft.fftfreq(len(x_values), x_values[1] - x_values[0])
-frequency_second = 0
+frequency_second = 5
 sampling_rate = 1000   # Гц
-duration = 1 # Время
-positive_freqs = frequency[:len(frequency)//2] + 5  
+duration = 5 # Время
+positive_freqs = frequency[:len(frequency)//2]  
 positive_amplitude = amplitude[:len(amplitude)//2]
-
+"""
 plt.figure(figsize=(12, 6))
 plt.plot(x_values, y_values, linewidth=2, color='blue')  
 plt.title('График функции синуса от 0 до 40π')
@@ -27,12 +27,12 @@ plt.xlabel('Частота')
 plt.ylabel('Амплитуда')
 plt.grid(True)
 plt.show()
-
+"""
 time = np.linspace(0, duration, int(sampling_rate * duration), endpoint=False)
-square_wave = np.sign(np.sin(2 * np.pi * frequency * time))
+square_wave = np.sign(np.sin(2 * np.pi * frequency_second* time))
 
 plt.figure(figsize=(10, 5))
-plt.plot(time, square_wave, label='Миандр')
+plt.plot(time, square_wave)
 plt.title('Миандр (прямоугольный сигнал)')
 plt.xlabel('Время (с)')
 plt.ylabel('Амплитуда')
